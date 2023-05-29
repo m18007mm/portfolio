e5b0001232ef1f06393c67d61da63ee6291e69df
hisato ando <m18007mm@gmail.com>
2023-05-30 02:28:54 +0900

    AboutComponentの追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
